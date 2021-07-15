3cdce57757ac91e13063ca9038cd3e2ebfeb6f67
sanobar28 <sanobar.mujawar@gmail.com>
2021-07-15 20:03:20 +0530

    [Sanobar]added: email input validation for html form

--- js/simple_form.js
+++ js/simple_form.js
@@ -19,6 +19,21 @@ text.addEventListener('input', function() {
     }
 })
 
+//UC2 email validation
+const email = document.querySelector('#email');
+const emailError = document.querySelector('.email-error');
+email.addEventListener('input', function () {
+    let emailRegex = RegExp("^[A-Za-z]{3,}([.+_-][A-Za-z0-9]+)*[@][A-Za-z0-9]{1,}[.][a-zA-Z0-9]{2,3}([.][0-9a-zA-Z]{2})?$");
+    if (emailRegex.test(email.value)) {
+        emailError.textContent = "";
+    }
+    else {
+        emailError.textContent = "Email is not Valid";
+    }
+});
+
+
+//salary range button synchronised with values of user
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
